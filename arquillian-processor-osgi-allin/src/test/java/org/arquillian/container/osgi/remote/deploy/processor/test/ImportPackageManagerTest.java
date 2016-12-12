4a88cde5aa50790785c0e69e13be61ce33dd2db0
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-12-12 09:05:14 +0100

    SF

--- arquillian-processor-osgi-allin/src/test/java/org/arquillian/container/osgi/remote/deploy/processor/test/ImportPackageManagerTest.java
+++ arquillian-processor-osgi-allin/src/test/java/org/arquillian/container/osgi/remote/deploy/processor/test/ImportPackageManagerTest.java
@@ -25,9 +25,6 @@ import com.liferay.arquillian.container.osgi.remote.processor.service.ImportPack
 import java.io.File;
 import java.io.IOException;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
