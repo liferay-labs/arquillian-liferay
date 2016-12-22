b9ebb5d285a6b4ba115eac2d64070a998396fe9b
Gregory Amerson <gregory.amerson@liferay.com>
2016-12-22 00:16:23 -0600

    suffix each of the JMXTestRunner Mbeans fixes #9ner/osgi/remote/activator/ArquillianBundleActivator.java

--- arquillian-processor-osgi-allin/src/main/java/com/liferay/arquillian/container/osgi/remote/activator/ArquillianBundleActivator.java
+++ arquillian-processor-osgi-allin/src/main/java/com/liferay/arquillian/container/osgi/remote/activator/ArquillianBundleActivator.java
@@ -34,6 +34,7 @@ import javax.management.MBeanServerFactory;
 
 import org.jboss.arquillian.protocol.jmx.JMXTestRunner;
 import org.jboss.arquillian.protocol.jmx.JMXTestRunner.TestClassLoader;
+import org.jboss.arquillian.protocol.jmx.JMXTestRunnerMBean;
 import org.jboss.arquillian.testenricher.osgi.BundleAssociation;
 import org.jboss.arquillian.testenricher.osgi.BundleContextAssociation;
 
@@ -70,7 +71,11 @@ public class ArquillianBundleActivator implements BundleActivator {
 
 		MBeanServer mbeanServer = _findOrCreateMBeanServer();
 
-		_testRunner = new JMXTestRunner(testClassLoader) {
+		String objectName =
+			JMXTestRunnerMBean.OBJECT_NAME + "-" +
+				context.getBundle().getSymbolicName();
+
+		_testRunner = new JMXTestRunner(testClassLoader, objectName) {
 
 			@Override
 			public byte[] runTestMethod(String className, String methodName) {
